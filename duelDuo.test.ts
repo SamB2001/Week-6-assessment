b0ee6bf09835391892bb66ce7a487c80fb9f6c0f
Sam Bischoff <sam.bischoff2001@gmail.com>
2022-07-01 12:52:30 -0600

    ahhhhhhh

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -22,14 +22,14 @@ test('Title shows up when page loads', async () => {
 
 test('Clicking draw button displays bots', async () => {
     await driver.findElement(By.id('draw')).click()
-    const displayedBots = await driver.findElement(By.xpath('//choices[@class="container"')).isDisplayed
+    const displayedBots = await driver.findElement(By.xpath('//choices[@class="container"]')).isDisplayed
     expect(displayedBots).toBeTruthy()
 })
 
 test('Add to Duo adds to player duo div', async () => {
     const addbots = await driver.findElement(By.id('choices'))
-    await addbots.findElement(By.xpath("//bot-card[@class='bot-btn'")).click()
-    const playerBots = await driver.findElement(By.xpath('//player-duo[@class="container"')).isDisplayed
+    await addbots.findElement(By.xpath("//bot-card outline[@class='bot-btn']")).click()
+    const playerBots = await driver.findElement(By.xpath('//player-duo[@class="container"]')).isDisplayed
     expect(playerBots).toBeTruthy()
 })
 
